6b455877e0037d77e639e6e0c2a16e76b6989c60
OnyxBot <michael.drogalis@onyxplatform.org>
2016-09-10 16:05:26 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-http "0.9.10.0-beta4"
+(defproject org.onyxplatform/onyx-http "0.9.10.0-SNAPSHOT"
   :description "Onyx plugin for HTTP"
   :url "https://github.com/onyx-platform/onyx-http"
   :repositories {"snapshots" {:url "https://clojars.org/repo"
