02b983555a1e98e96a818cac13c14c55d197d139
Lucas Bradstreet <lucasbradstreet@gmail.com>
2018-01-22 18:09:21 -0800

    Release version 0.12.4.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-http "0.12.4.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-http "0.12.4.1"
   :description "Onyx plugin for HTTP"
   :url "https://github.com/onyx-platform/onyx-http"
   :repositories {"snapshots" {:url "https://clojars.org/repo"
