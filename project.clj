7b2722ff091f1841872d18f4a985267629324502
OnyxBot <michael.drogalis@onyxplatform.org>
2018-05-15 16:47:14 +0000

    Update dependency org.onyxplatform/onyx to version 0.13.0-20180515_163858-g606b217.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.13.0-20180515_063819-g3d02fc4"]
+                 [org.onyxplatform/onyx "0.13.0-20180515_163858-g606b217"]
                  [aleph "0.4.4"]
                  [io.netty/netty-all "4.1.12.Final"]]
   :profiles {:dev {:dependencies [[cheshire "5.8.0"]]
